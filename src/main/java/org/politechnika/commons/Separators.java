d4031efbf301118473e5c8370ca1e72d549d7a42
Igor Kopeć <igor.kopec@edge1s.com>
2019-03-03 20:29:00 +0100

    separator

--- src/main/java/org/politechnika/commons/Separators.java
+++ src/main/java/org/politechnika/commons/Separators.java
@@ -3,4 +3,5 @@ package org.politechnika.commons;
 public final class Separators {
     public static final char PIPE = '|';
     public static final char SPACE = ' ';
+    public static final String TAB = "\t";
 }
