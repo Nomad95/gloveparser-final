c43067b63897016c569a28f3263eff32b3ababa9
Tobiasz K <tkaciuczyk@gmail.com>
2019-03-19 22:21:20 +0100

    PoC implementacji algorytmu pulsometru

--- src/main/java/org/politechnika/report/impl/PulsometerReportGenerator.java
+++ src/main/java/org/politechnika/report/impl/PulsometerReportGenerator.java
@@ -1,13 +1,42 @@
 package org.politechnika.report.impl;
 
+import org.politechnika.analysis.impl.PulsometerStatisticAnalyzerImpl;
 import org.politechnika.commons.Constants;
+import org.politechnika.data_parser.csv.definitions.GloveParsingStrategy;
+import org.politechnika.data_parser.csv.definitions.PulsometerParsingStrategy;
+import org.politechnika.data_parser.csv.definitions.beans.PulsometerDataDto;
+import org.politechnika.data_parser.csv.impl.BeanCsvParser;
 import org.politechnika.file.model.AbstractDataFile;
 import org.politechnika.report.ReportGenerator;
+
+import java.io.FileNotFoundException;
+import java.util.List;
 
 public class PulsometerReportGenerator implements ReportGenerator {
     @Override
     public void generate(AbstractDataFile dataFile) {
-        //TODO: load data -> process and aggregate -> save to cache if needed for further processing -> save result to file (LAZY!)-> generate and save charts
+
+        BeanCsvParser beanCsvParser = new BeanCsvParser();
+        List<PulsometerDataDto> pulsometerDataDtos;
+        try {
+            pulsometerDataDtos = beanCsvParser.parseToBean(dataFile, new PulsometerParsingStrategy());
+        } catch (FileNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+
+        PulsometerStatisticAnalyzerImpl analyzer = new PulsometerStatisticAnalyzerImpl();
+        double average = analyzer.getAverage(pulsometerDataDtos, PulsometerDataDto::getValue);
+        double kurtosis = analyzer.getKurtosis(pulsometerDataDtos, PulsometerDataDto::getValue);
+        double skewness = analyzer.getSkewness(pulsometerDataDtos, PulsometerDataDto::getValue);
+        double deviation = analyzer.getStandardDeviation(pulsometerDataDtos, PulsometerDataDto::getValue);
+        double variance = analyzer.getVariance(pulsometerDataDtos, PulsometerDataDto::getValue);
+
+
+        //TODO: load data
+        // -> process and aggregate
+        // -> save to cache if needed for further processing
+        // -> save result to file (LAZY!)
+        // -> generate and save charts
     }
 
     @Override
